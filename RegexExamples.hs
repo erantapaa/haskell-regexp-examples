df8ea870cce0082c270516ccb053357deaffe8e8
Erik Rantapaa <erantapaa@gmail.com>
2015-10-16 07:27:21 -0500

    Renamed examples.

--- RegexExamples.hs
+++ RegexExamples.hs
@@ -2,66 +2,53 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 module RegexExamples where
-{-
 
-This file contains examples of how to use all 27 instances of RegexContext.
-
-A RegexContext determines what information the match operator =~ returns and
-how that information is structured.
-
-The match information which can be selected by using the various instances
-include:
-
-- which matches to report on (just the first match or all matches)
-- which captures to return (i.e. only the 0-th or all captures)
-- which details about each capture to return (the match text, offset, length)
-- whether to return the before and after text
-
-To use this file first uncomment the appropriate import lines below to select
-which regex package you want to use. See the HaskellWiki page for a comparison
-of all of the regex packages:
-
-  http://www.haskell.org/haskellwiki/Regular_expressions
-
-Each example has a "Context:" description which indicates which RegexContext instance the
-example uses. All of the Regexp instances may be found in in the Text.Regex.Base.RegexLike
-module:
-
-  http://hackage.haskell.org/package/regex-base/docs/Text-Regex-Base-RegexLike.html#t:RegexContext
-
--}
+--
+-- This file contains runnable examples of how to use all 27 instances of RegexContext.
+--
 
 import Control.Monad
 import Data.Array
 import qualified Data.Text as T
 
--- for regexp-posix:
--- import Text.Regex.Posix
+--
+-- regex package selection
+--
 
--- for regex-tdfa:
--- import Text.Regex.TDFA
--- and possibly: Text.Regex.TDFA.Text
+-- import Text.Regex.Posix       -- regex-posix
+-- import Text.Regex.TDFA        -- regex-tdfa
+-- import Text.Regex.TDFA.Text   -- regex-tdfa-text
 
--- for regex-pcre / regex-pcre-builtin
-import Text.Regex.PCRE
+import Text.Regex.PCRE           -- regex-pcre / regex-pcre-builtin
+
 
 str = "a bbbb aa b" :: String
 regex = "(a+) (b+)" :: String
 
 -- bool: first match, returns True or False
-boolExample = 
+exampleBool = do
   if str =~ regex then putStrLn "matched" else putStrLn "no matches"
 -- Context: RegexContext a b Bool 
 
 -- count: all matches, returns count
-countExample =
+exampleCount = do
   if (str =~ regex :: Int) > 3
     then putStrLn "matched more than 3 times"
     else putStrLn "did not match more than 3 times"
 -- Context: RegexContext a b Int
 
--- match-array: 1st match, all captures, returns offset and length
-matchArrayExample = do
+-- null: always returns ()
+exampleNull = do
+  let matches = str =~ regex :: ()
+  print matches
+-- Context: RegexContext a b ()
+
+--
+-- First match only using MatchArray and MatchResult.
+--
+
+-- first match, all captures, returns offset and length
+exampleMatchArray = do
   let matches = str =~ regex :: MatchArray
       (lo,hi) = bounds matches
   if hi > 0 then putStrLn $ "matched and capture 1 has offset and length " ++ (show $ matches ! 1)
@@ -69,145 +56,160 @@ matchArrayExample = do
   print matches
 -- Context: RegexContext a b MatchArray
 
--- lists-of-lists: all matches, all captures, returns text
-listOfListsExample = do
-  let matches = str =~ regex :: [[String]]
-  print matches
--- Context: RegexContext a b [[b]]
-
--- list-of-matchtext: all matches, all captures, returns text, offset and length
-listOfMatchTextExample = do
-  let matches = str =~ regex :: [MatchText String]
-  print matches
--- Context: RegexContext a b [MatchText b]
-
--- list-of-matcharray: all matches, all captures, returns offset and length
-listOfMatchArrayyExample = do
-  let matches = str =~ regex :: [MatchArray]
-  print matches
--- Context: RegexContext a b [MatchArray]
-
--- matchresult: 1st match, all captures, returns text, includes before and after text
-matchResultExample = do
+-- first match, all captures, returns text, includes before and after text
+exampleMatchResult = do
   let matches = str =~ regex :: MatchResult String
   -- Note: there is no Show instance for MatchResult
-  putStrLn $ "before text         :" ++ (mrBefore matches)
-  putStrLn $ "after text          :" ++ (mrAfter matches)
-  putStrLn $ "match text          :" ++ (mrMatch matches)
-  putStrLn $ "captures 1-n (list) :" ++ show (mrSubList matches)
-  putStrLn $ "all captures (array):" ++ show (mrSubs matches)
+  putStrLn $ "before text         : " ++ (mrBefore matches)
+  putStrLn $ "after text          : " ++ (mrAfter matches)
+  putStrLn $ "match text          : " ++ (mrMatch matches)
+  putStrLn $ "captures 1-n (list) : " ++ show (mrSubList matches)
+  putStrLn $ "all captures (array): " ++ show (mrSubs matches)
 -- Context: RegexContext a b (MatchResult b)
 
--- alltextmatches-array-array: all matches, all captures, returns text
-allTextMatchesArrayArrayExample = do
-  let matches = getAllTextMatches (str =~ regex) :: Array Int (Array Int String)
-  print matches
--- Context: RegexContext a b (AllTextMatches (Array Int) (Array Int b))
+--
+-- First match only using getAllTextSubmatches.
+--
 
--- alltextmatches-list-array: all matches, all captures, returns text
-allTextMatchesListArrayExample = do
-  let matches = getAllTextMatches $ str =~ regex :: [Array Int String]
-  print matches
--- Context: RegexContext a b (AllTextMatches [] (Array Int b))
-
--- alltextmatches-array-list: all matches, all captures, returns text
-allTextMatchesArrayListExample = do
-  let matches = getAllTextMatches $ str =~ regex :: Array Int [String]
-  print matches
--- Context: RegexContext a b (AllTextMatches (Array Int) [b])
-
--- alltextmatches-array-text: all matches, 0-th capture, returns text
-allTextMatchesArrayTextExample = do
-  let matches = getAllTextMatches $ str =~ regex :: Array Int String
-  print matches
--- Context: RegexContext a b (AllTextMatches (Array Int) b)
-
--- alltextmatches-list-text: all matches, 0-th capture, return text
-allTextMatchesListTextExample = do
-  let matches = getAllTextMatches $ str =~ regex :: [String]
-  print matches
--- Context: RegexContext a b (AllTextMatches [] b)
-
--- alltextmatches-array-matchtext: all matches, all captures, returns text, offset and length
-allTextMatchesArrayMatchTextExample = do
-  let matches = getAllTextMatches $ str =~ regex :: Array Int (MatchText String)
-  print matches
--- Context: RegexContext a b (AllTextMatches (Array Int) (MatchText b))
-
--- allmatches-array-matcharray: all matches, all captures, returns offset and length
-allMatchesArrayMatchArrayExample = do
-  let matches = getAllMatches $ str =~ regex :: Array Int MatchArray
-  print matches
--- Context: RegexContext a b (AllMatches (Array Int) MatchArray)
-
--- allmatches-array-offset-length: all matches, 0-th capture, returns offset and length
-allMatchesArrayOffsetLengthExample = do
-  let matches = getAllMatches $ str =~ regex :: Array Int (MatchOffset,MatchLength)
-  print matches
--- Context: RegexContext a b (AllMatches (Array Int) (MatchOffset, MatchLength))
-
--- allmatches-list-offset-length: all matches, 0-th capture, returns offset and length
-allMatchesListOffsetLengthExample = do
-  let matches = getAllMatches $ str =~ regex :: [(MatchOffset,MatchLength)]
-  print matches
--- Context: RegexContext a b (AllMatches [] (MatchOffset, MatchLength))
-
--- alltextsubmatches-array-text: 1st match, all captures, returns text
-allTextSubmatchesArrayTextExample = do
+-- first match, all captures, returns text
+exampleAllTextSubmatchesArrayText = do
   let matches = getAllTextSubmatches $ str =~ regex :: Array Int String
   print matches
 -- Context: RegexContext a b (AllTextSubmatches (Array Int) b)
 
--- alltextsubmatches-list-text: 1st match, all captures, returns text
-allTextSubmatchesListTextExample = do
+-- first match, all captures, returns text
+exampleAllTextSubmatchesListText = do
   let matches = getAllTextSubmatches $ str =~ regex :: [String]
   print matches
 -- Context: RegexContext a b (AllTextSubmatches [] b)
 
--- alltextsubmatches-list-text-offset-length: 1st match, all captures, returns text, offset and length
-allTextSubmatchesListTextOffsetLengthExample = do
+-- first match, all captures, returns text, offset and length
+exampleAllTextSubmatchesListTextOffsetLength = do
   let matches = getAllTextSubmatches $ str =~ regex :: [(String,(MatchOffset,MatchLength))]
   print matches
 -- Context: RegexContext a b (AllTextSubmatches [] (b, (MatchOffset, MatchLength)))
 
--- altextsubmatches-array-text-offset-length: 1st match, all captures, returns text, offset and length
-allTextSubmatchesArrayTextOffsetLengthExample = do
+-- first match, all captures, returns text, offset and length
+exampleAllTextSubmatchesArrayTextOffsetLength = do
   let matches = getAllTextSubmatches $ str =~ regex :: Array Int (String,(MatchOffset,MatchLength))
   print matches
 -- Context: RegexContext a b (AllTextSubmatches (Array Int) (b, (MatchOffset, MatchLength)))
 
--- allsubmatches-list-offset-length: 1st match, all captures, returns offset and length
-allSubmatchesListOffsetLengthExample = do
+-- first match, all captures, returns offset and length
+exampleAllSubmatchesListOffsetLength = do
   let matches = getAllSubmatches $ str =~ regex :: [(MatchOffset,MatchLength)]
   print matches
 -- Context: RegexContext a b (AllSubmatches [] (MatchOffset, MatchLength))
 
--- offset-length: 1st match, 0-th capture, returns offset and length
-offsetLengthExample = do
+--
+-- First match only using tuples.
+--
+
+-- first match, 0-th capture, returns offset and length
+exampleOffsetLength = do
   let matches = str =~ regex :: (MatchOffset,MatchLength)
   print matches
 -- Context: RegexContext a b (MatchOffset, MatchLength)
 
--- beforeMatchAfter:  1st match, 0-th capture, returns text including before and after text
-beforeMatchAfterExample = do
+-- first match, 0-th capture, returns text including before and after text
+exampleBeforeMatchAfter = do
   let matches = str =~ regex :: (String,String,String)
   print matches
 -- Context: RegexContext a b (b, b, b)
 
--- beforeMatchTextAfter: 1st match, all captures, returns text, offset and length including before and after text
-beforeMatchTextAfterExample = do
+-- first match, all captures, returns text, offset and length including before and after text
+exampleBeforeMatchTextAfter = do
   let matches = str =~ regex :: (String,MatchText String,String)
   print matches
 -- Context: RegexContext a b (b, MatchText b, b)
 
--- beforeMatchAfterWithCaptures: 1st match, all captures, returns text
-beforeMatchAfterWithCapturesExample = do
+-- first match, all captures, returns text
+exampleBeforeMatchAfterWithCaptures = do
   let matches = str =~ regex :: (String,String,String,[String])
   print matches
 -- Context: RegexContext a b (b, b, b, [b])
 
--- null: always returns ()
-nullExample = do
-  let matches = str =~ regex :: ()
+--
+-- All matches returning a list.
+--
+
+-- all matches, all captures, returns text
+exampleListOfLists = do
+  let matches = str =~ regex :: [[String]]
   print matches
+-- Context: RegexContext a b [[b]]
+
+-- all matches, all captures, returns text, offset and length
+exampleListOfMatchesText = do
+  let matches = str =~ regex :: [MatchText String]
+  print matches
+-- Context: RegexContext a b [MatchText b]
+
+-- all matches, all captures, returns offset and length
+exampleListOfMatchesArray = do
+  let matches = str =~ regex :: [MatchArray]
+  print matches
+-- Context: RegexContext a b [MatchArray]
+
+--
+-- All matches using getAllTextMatches
+--
+
+-- all matches, all captures, returns text
+exampleAllTextMatchesArrayArray = do
+  let matches = getAllTextMatches (str =~ regex) :: Array Int (Array Int String)
+  print matches
+-- Context: RegexContext a b (AllTextMatches (Array Int) (Array Int b))
+
+-- all matches, all captures, returns text
+exampleAllTextMatchesListArray = do
+  let matches = getAllTextMatches $ str =~ regex :: [Array Int String]
+  print matches
+-- Context: RegexContext a b (AllTextMatches [] (Array Int b))
+
+-- all matches, all captures, returns text
+exampleAllTextMatchesArrayList = do
+  let matches = getAllTextMatches $ str =~ regex :: Array Int [String]
+  print matches
+-- Context: RegexContext a b (AllTextMatches (Array Int) [b])
+
+-- all matches, 0-th capture, returns text
+exampleAllTextMatchesArrayText = do
+  let matches = getAllTextMatches $ str =~ regex :: Array Int String
+  print matches
+-- Context: RegexContext a b (AllTextMatches (Array Int) b)
+
+-- all matches, 0-th capture, return text
+exampleAllTextMatchesListText = do
+  let matches = getAllTextMatches $ str =~ regex :: [String]
+  print matches
+-- Context: RegexContext a b (AllTextMatches [] b)
+
+-- all matches, all captures, returns text, offset and length
+exampleAllTextMatchesArrayMatchText = do
+  let matches = getAllTextMatches $ str =~ regex :: Array Int (MatchText String)
+  print matches
+-- Context: RegexContext a b (AllTextMatches (Array Int) (MatchText b))
+
+--
+-- All matches using getAllMatches
+--
+
+-- all matches, all captures, returns offset and length
+exampleAllMatchesArrayMatchArray = do
+  let matches = getAllMatches $ str =~ regex :: Array Int MatchArray
+  print matches
+-- Context: RegexContext a b (AllMatches (Array Int) MatchArray)
+
+-- all matches, 0-th capture, returns offset and length
+exampleAllMatchesArrayOffsetLength = do
+  let matches = getAllMatches $ str =~ regex :: Array Int (MatchOffset,MatchLength)
+  print matches
+-- Context: RegexContext a b (AllMatches (Array Int) (MatchOffset, MatchLength))
+
+-- all matches, 0-th capture, returns offset and length
+exampleAllMAtchesListOffsetLength = do
+  let matches = getAllMatches $ str =~ regex :: [(MatchOffset,MatchLength)]
+  print matches
+-- Context: RegexContext a b (AllMatches [] (MatchOffset, MatchLength))
 
